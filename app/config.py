388100b7eb1b78ccc0a0421d35418f0dcc3f4557
Dante_Chen <dante_chen@aseglobal.com>
2025-06-24 11:34:15 +0800

    feat(config): 新增 SOAP_URL 設定以支援 SOAP 服務

--- app/config.py
+++ app/config.py
@@ -5,6 +5,10 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 
 class Config:
+    """Configuration class for the application."""
+    # General settings
+    SOAP_URL = "http://10.12.20.216/mtlserviceproxy/serviceproxy.asmx"
+    
     MSSQL_HOST = os.getenv("MSSQL_HOST", "localhost")
     MSSQL_PORT = os.getenv("MSSQL_PORT", "1433")
     MSSQL_USER = os.getenv("MSSQL_USER", "sa")
